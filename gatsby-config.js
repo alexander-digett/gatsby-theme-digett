9364ff797414d89d111728e2b5612579d39386ee
Alexander Fountain <alexander@digett.com>
2019-04-26 18:25:09 -0500

    basic page set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,27 @@
 const path = require("path")
 
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Theme Digett`,
+    description: `Digett's Gatsby Theme`,
+    siteUrl: `https://www.digett.com`,
+    author: `@alexanderfountain`,
+    logo: `/assets/logo_no_comp.png`,
+    menuLinks:[
+      {
+         name:'Link 1',
+         link:'/link-1'
+      },
+      {
+         name:'Link 2',
+         link:'/link-2'
+      },
+      {
+         name:'Link 3',
+         link:'/link-3'
+      },
+    ],
+  },
   plugins: [
     {
       resolve: "gatsby-plugin-page-creator",
@@ -13,6 +34,33 @@ module.exports = {
       options: {
         // replace with the name of your theme
         modules: ["gatsby-theme-digett"],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: `pages`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 800,
+            },
+          },
+        ],
       },
     },
     `gatsby-v2-plugin-page-transitions`,
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-import React from "react"
-
-export default () => <h1>Hello from gatsby-theme-digett</h1>
