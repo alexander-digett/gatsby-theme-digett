49315818f75e04ec49619df7cfbe87c102d8a7e3
Alexander Fountain <alexander@digett.com>
2019-04-29 14:58:15 -0500

    added netlify gatsby plugin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,13 @@ import CMS, { init } from 'netlify-cms'
 /**
  * Optionally pass in a config object. This object will be merged into `config.yml` if it exists
  */
+ var collections = [      
+ { label: "Basic Page 6", name: "basic-page-6", folder: "src/pages", create: true, fields: [
+        { label: "Template Key", name: "templateKey", widget: "hidden", default: "basic-page" },
+        { label: "Title2", name: "title", widget: "string" },
+        { label: "Body", name: "body", widget: "markdown" },
+      ]}
+  ];
 
 init({
   config: {
@@ -12,12 +19,6 @@ init({
     },
     media_folder: "static/assets",
     public_folder: "/assets",
-    collections: [
-      { label: "Basic Page 3", name: "basic-page-3", folder: "src/pages", create: true, fields: [
-        { label: "Template Key", name: "templateKey", widget: "hidden", default: "basic-page" },
-        { label: "Title2", name: "title", widget: "string" },
-        { label: "Body", name: "body", widget: "markdown" },
-      ]},
-    ],
+    collections: collections,
   },
 })
