92838f4c7f03e64f007cf29109ad376cb937d09d
Alexander Fountain <alexander@digett.com>
2019-04-29 13:37:02 -0500

    config comment out

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -9,5 +9,30 @@ init({
     backend: {
       name: "git-gateway",
     },
+    collections:{
+    	name: "basic-page",
+    	label: "Basic Page",
+    	folder: "src/pages",
+    	create: true,
+    	slug: "{{year}}-{{month}}-{{day}}-{{slug}}",
+    	fields:{
+    		[
+    		label: "Template Key", 
+    		name: "templateKey", 
+    		widget: "hidden", 
+    		default: "basic-page"
+    		],
+    		[
+    		label: "Title", 
+    		name: "title", 
+    		widget: "string"
+    		],
+    		[
+    		label: "Body", 
+    		name: "body", 
+    		widget: "markdown"
+    		],
+    	}
+    }
   },
 })
