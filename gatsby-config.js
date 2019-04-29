bdcc26c41f9370aef769ea4fbff84722f2bc4c82
Alexander Fountain <alexander@digett.com>
2019-04-29 09:58:48 -0500

    source from templates added

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: path.join(__dirname, "src", "templates"),
+      },
+    },   
+    {
       resolve: "gatsby-plugin-compile-es6-packages",
       options: {
         // replace with the name of your theme
