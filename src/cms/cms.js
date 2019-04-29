4004c37b96132ea98516d0500064fcd66b31e998
Alexander Fountain <alexander@digett.com>
2019-04-29 14:08:36 -0500

    removed admin config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -13,9 +13,9 @@ init({
     media_folder: "static/assets",
     public_folder: "/assets",
     collections: [
-      { label: "Basic Page", name: "basic-page", folder: "src/pages", create: true, fields: [
+      { label: "Basic Page 3", name: "basic-page-3", folder: "src/pages", create: true, fields: [
         { label: "Template Key", name: "templateKey", widget: "hidden", default: "basic-page" },
-        { label: "Title", name: "title", widget: "string" },
+        { label: "Title2", name: "title", widget: "string" },
         { label: "Body", name: "body", widget: "markdown" },
       ]},
     ],
